b31fe37fee12b9779ad46206e025d0b78bbb7536
Jacob.Wilde <jacob.wilde@utah.edu>
2020-10-05 12:37:06 -0600

    updating card styling

--- utils/CreateHTML.js
+++ utils/CreateHTML.js
@@ -1,16 +1,25 @@
 function CreateHTML(empArray) {
     console.log('made it')
-    return `<!DOCTYPE html>
+
+    let a = `<!DOCTYPE html>
     <html lang="en">
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Document</title>
-    </head>
-    <body>
-        
+    </head>`
+
+    let b = 
+    `   <body>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"></script>
     </body>
     </html>`
+
+    let c = a + '\r' + b
+
+    return c
+     
+    
 };
 
 module.exports = CreateHTML
